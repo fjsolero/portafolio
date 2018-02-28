d7140213ca7908f9f952bad98d416da952c4567d
Javier Solero <fjsolero@gmail.com>
2018-02-28 16:42:50 +0100

    Servicio funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 //Rutas
 import { app_routing } from "./app.routes";
+//Servicios
+import { InformacionService }  from "./services/informacion.service";
 //Componentes
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -22,9 +25,12 @@ import { ItemComponent } from './components/item/item.component';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     app_routing
   ],
-  providers: [],
+  providers: [
+    InformacionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
