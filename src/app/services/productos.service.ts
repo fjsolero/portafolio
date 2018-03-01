4969da4f45d749fb691646b568baf0eca4f535f7
Javier Solero <fjsolero@gmail.com>
2018-03-01 12:38:08 +0100

    Agregamos parametro al item

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -16,9 +16,11 @@ export class ProductosService {
       this.cargando=true;
       this.http.get("https://paginaweb-fc0fd.firebaseio.com/productos_idx.json").subscribe(
         res => {
-          console.log(res.json());
-          this.cargando=false;
-          this.productos=res.json();
+          // console.log(res.json());
+          // setTimeout( () => {
+            this.cargando=false;
+            this.productos=res.json();
+          // }, 1500)
       })
   }
 
