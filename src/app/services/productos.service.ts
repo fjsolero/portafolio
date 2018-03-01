521f41bb78b52554e460dd50f95cea58d00e8d51
Javier Solero <fjsolero@gmail.com>
2018-03-01 19:50:55 +0100

    TERMINADO

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -6,10 +6,34 @@ import { Http } from "@angular/http";
 export class ProductosService {
 
   productos:any[]=[];
+  productos_filtrado:any[]=[];
   cargando:boolean = true;
 
   constructor( private http:Http) {
     this.cargar_productos();
+  }
+
+  public buscar_producto( termino:string ){
+    if(this.productos.length===0){
+      this.cargar_productos().then( ()=>{
+        //Terminó la carga
+        this.filtrar_productos(termino);
+      });
+    } else {
+      this.filtrar_productos(termino);
+    }
+  }
+
+  private filtrar_productos( termino:string ){
+    this.productos_filtrado=[];
+    termino=termino.toLowerCase();
+    this.productos.forEach( prod => {
+      if( prod.categoria.indexOf(termino) >= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0 ){
+        this.productos_filtrado.push( prod );
+      }
+      // console.log( prod );
+    })
+
   }
 
   public cargar_producto( cod:string){
@@ -18,14 +42,18 @@ export class ProductosService {
 
   public cargar_productos(){
       this.cargando=true;
-      this.http.get("https://paginaweb-fc0fd.firebaseio.com/productos_idx.json").subscribe(
-        res => {
-          // console.log(res.json());
-          // setTimeout( () => {
+      let promesa = new Promise( ( resolve, reject )=>{
+        this.http.get("https://paginaweb-fc0fd.firebaseio.com/productos_idx.json").subscribe(
+          res => {
+            // console.log(res.json());
+            // setTimeout( () => {
             this.cargando=false;
             this.productos=res.json();
-          // }, 1500)
-      })
+            resolve();
+            // }, 1500)
+          });
+      });
+      return promesa;
   }
 
 }
